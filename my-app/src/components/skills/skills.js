ec8017d6b4fe033eda18bb51ac93caef8844aead
bttodd11 <bttodd@icloud.com>
2024-03-26 08:15:31 -0500

    Still working on skill section

--- my-app/src/components/skills/skills.js
+++ my-app/src/components/skills/skills.js
@@ -7,10 +7,10 @@ const Skills = () => {
 
          <div className='skillWrapper'>
             <p id="contentTitle">Skills</p>
-            <p className='skillsLabel'>Front-End</p>
-            <p className='skillsLabel'>Back-End</p>
-            <p className='skillsLabel'>Database</p>
-            <p className='skillsLabel'>Other</p>
+            <p className='skillsLabel'>Front-End : <span className='skillsText' >React, Vue</span></p>
+            <p className='skillsLabel'>Back-End : <span className='skillsText' >React, Vue</span></p>
+            <p className='skillsLabel'>Database : <span className='skillsText' >React, Vue</span></p>
+            <p className='skillsLabel'>Other : <span className='skillsText' >React, Vue</span></p>
             </div>
 
    )
