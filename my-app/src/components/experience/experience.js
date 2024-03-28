c28fecc9c0626636d3af0591c2f03e9f563f774a
bttodd11 <bttodd@icloud.com>
2024-03-27 22:35:06 -0500

    Working on styling updates

--- my-app/src/components/experience/experience.js
+++ my-app/src/components/experience/experience.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
+import CompanyLogo from "../image/companyLogo.png";
+import "../experience/experience.css";
 
 const Experience = () => {
-return (
-   <div>
- <p id='contentTitle'>Experience</p>
+  return (
+    <div className="experienceWrapper">
+      <p id="contentTitle">Experience</p>
 
-   </div>
-)
+
+      <div className="companyContainer">
+      <img src={CompanyLogo} className="companyLogo" /> 
+      <p className="companyText">Company 1</p>
+      <p className="experienceText">Jan 2000 - Feb 2007</p>
+      </div>
+            <div id="divider"></div>
+
+
+      <div className="companyContainer">
+      <img src={CompanyLogo} className="companyLogo" /> 
+      <p className="companyText">Company 1</p>
+      <p className="experienceText">Jan 2000 - Feb 2007</p>
+      </div>
+
+
+      <div className="companyContainer">
+      <img src={CompanyLogo} className="companyLogo" /> 
+      <p className="companyText">Company 1</p>
+      <p className="experienceText">Jan 2000 - Feb 2007</p>
+      </div>
+    </div>
+  );
 };
-
 
 export default Experience;
--- my-app/src/components/skills/skills.js
+++ my-app/src/components/skills/skills.js
@@ -7,10 +7,10 @@ const Skills = () => {
 
          <div className='skillWrapper'>
             <p id="contentTitle">Skills</p>
-            <p className='skillsLabel'>Front-End : <span className='skillsText' >React, Vue</span></p>
-            <p className='skillsLabel'>Back-End : <span className='skillsText' >React, Vue</span></p>
-            <p className='skillsLabel'>Database : <span className='skillsText' >React, Vue</span></p>
-            <p className='skillsLabel'>Other : <span className='skillsText' >React, Vue</span></p>
+            <p className='skillsLabel'>Front-End : <span className='skillsText' >React, Angular, Vue, Bootstrap, Sass, </span></p>
+            <p className='skillsLabel'>Back-End : <span className='skillsText' >Java, Python, Spring, Springboot, C#, Kotlin, Node.js</span></p>
+            <p className='skillsLabel'>Database : <span className='skillsText' >MySQL, MongoDB, Redis, PostgreSQL</span></p>
+            <p className='skillsLabel'>Other : <span className='skillsText' >Google Cloud, Amazon Web Services, Big Query, Compute Engine</span></p>
             </div>
 
    )
