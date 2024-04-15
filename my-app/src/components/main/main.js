10fa8d87c8bd6dae01d7225602b3b2f4bacdb816
bttodd11 <bttodd@icloud.com>
2024-04-14 22:06:46 -0500

    Working with framer motion

--- my-app/src/components/main/main.js
+++ my-app/src/components/main/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImagePlaceHolder from '../image/imagePlaceHolder.jpeg'
 import Headers from '../headers/headers';
+import { motion } from "framer-motion"
 import "./main.css";
 
 const Main = () => {
@@ -9,7 +10,25 @@ const Main = () => {
       <div id="mainSection">
          <p className='mainTitle'>FirstName.LastName</p>
          <span className='cityTitle'>City,</span><span className='stateTitle'>State</span>
-         <img src={ImagePlaceHolder} className='portfolioImage box' alt='Portfolio' /> 
+         <motion.div       
+      animate={{
+        scale: [.2, .8, .5, .3, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+        opacity: [.6, .4, .2,.7,1]
+      }}
+       transition={{
+        duration: 2,
+        ease: "easeIn",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1,
+        repeatType: "mirror"
+      }}
+  
+
+         >
+         <img src={ImagePlaceHolder} className='portfolioImage box' alt='Portfolio' /></motion.div>
          < Headers />
       </div>
    )
